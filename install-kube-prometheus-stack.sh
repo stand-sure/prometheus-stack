2ab2c6301e3116ad2a2dfa5bf332c700918a1daf
C. J. Anderson <chris@standsure.io>
2023-02-15 15:04:23 -0500

    added -x for output

--- install-kube-prometheus-stack.sh
+++ install-kube-prometheus-stack.sh
@@ -1,2 +1,2 @@
-#! /bin/bash
+#! /bin/bash -x
 helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace --values value-overrides.yaml
